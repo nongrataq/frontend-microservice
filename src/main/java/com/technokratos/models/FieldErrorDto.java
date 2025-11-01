59d6316ceb9f3c3351d3196f7f1c55d50e2223d6
Bogdan <bogdan-747@mail.ru>
2025-11-01 10:09:20 +0300

    base settings

---
+++ src/main/java/com/technokratos/models/FieldErrorDto.java
@@ -0,0 +1,6 @@
+package com.technokratos.models;
+
+public class FieldErrorDto {
+    String message;
+    String field;
+}
---
+++ src/main/java/com/technokratos/servlets/SignUpServlet.java
@@ -0,0 +1,4 @@
+package com.technokratos.servlets;
+
+public class SignUpServlet {
+}
