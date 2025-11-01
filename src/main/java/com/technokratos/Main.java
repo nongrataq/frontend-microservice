dc7edb61cd627dbe5621f572ef031c6f637c7289
Bogdan <bogdan-747@mail.ru>
2025-11-01 09:25:20 +0300

    start

---
+++ src/main/java/com/technokratos/Main.java
@@ -0,0 +1,10 @@
+package com.technokratos;
+
+import okhttp3.OkHttpClient;
+
+public class Main {
+    public static void main(String[] args) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+    }
+
+}
